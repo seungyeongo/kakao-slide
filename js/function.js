f7a1715846ba5826a509ec2282e1f0edbc9034d7
승연 <ko1020@kakao.com>
2023-08-11 14:37:56 +0900

    kakao-silde

---
+++ js/function.js
@@ -0,0 +1,45 @@
+const $indicator = $('.gnb>li');
+const $container = $('.slides-container');
+
+let nowIdx = 0;
+
+const moveFn = function () {
+	$container.animate({ left: -100 * nowIdx + '%' });
+
+	$indicator.eq(nowIdx).addClass('on');
+	$indicator.eq(nowIdx).siblings().removeClass('on');
+};
+
+$indicator.on('click', function (evt) {
+	evt.preventDefault();
+
+	nowIdx = $indicator.index(this);
+
+	moveFn();
+});
+
+$('.slides-next').on('click', function (evt) {
+	evt.preventDefault();
+
+	$;
+
+	if (nowIdx < $indicator.length - 1) {
+		nowIdx++;
+	} else {
+		nowIdx = 0;
+	}
+
+	moveFn();
+});
+
+$('.slides-prev').on('click', function (evt) {
+	evt.preventDefault();
+
+	if (nowIdx > 0) {
+		nowIdx--;
+	} else {
+		nowIdx = $indicator.length - 1;
+	}
+
+	moveFn();
+});
